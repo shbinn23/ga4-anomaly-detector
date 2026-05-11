473bee2a17d8df0f96a2d8c271238f7184c8c9dd
shbinn <shbinn23@gmail.com>
2026-05-11 20:48:14 +0900

    chore: 고도화 관련 파일 및 인프라 제거 (dev 브랜치 초기화)

--- app/core/dependencies.py
+++
@@ -1,9 +0,0 @@
-from ..services.anomaly_service import AnomalyService
-from ..ml.prophet_detector import ProphetDetector
-from ..infrastructure.json_storage import JSONStorage
-
-def get_anomaly_service() -> AnomalyService:
-    """FastAPI Depends를 위한 의존성 주입 함수입니다."""
-    detector = ProphetDetector()
-    storage = JSONStorage()
-    return AnomalyService(detector=detector, storage=storage)
